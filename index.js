d5eeea80eb2b8b5c272fc3e6a56b2824405466b6
Marcus <104134633+MirkusX@users.noreply.github.com>
2022-08-26 14:14:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const todoListContainer = document.querySelector("#list")
 const todoSearch = document.querySelector("#todoSearch")
 const nothing = document.querySelector("#nothing")
 let todoList = [];
+//gets html elements
 
+//gets stored data in localstorage
 if (localStorage.length >= 1) {
   let saved = JSON.parse(localStorage.getItem("list"));
   saved.forEach((e) => {
@@ -17,16 +19,19 @@ if (localStorage.length >= 1) {
   console.log(todoList);
 }
 
+//detects if user presses enter and starts addInitial()
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
    addInitial()
   }
 })
 
+//detects if use presses the todoAdd button and starts addInitial()
 todoAdd.addEventListener("click", () => {
   addInitial()
 })
 
+//checks if input has text, if it doesnt it returns the function and displays error message
 function addInitial() {
   if (todoInput.value === "") {
     error.classList.remove("hidden")
@@ -41,15 +46,16 @@ function addInitial() {
     nothing.classList.remove("hidden")
     nothing.classList.add("show")
   }
-  
+  //pushes both input value and date which acts as id to array for storage
   let itemId = Date.now()
   todoList.push({ text: todoInput.value, id: itemId });
   addList();
 };
-
+//creates li and span, pushes array.text to li textcontent and array.id for its id. pushes array.id into span for its textcontent
 function addList() {
   const todoElement = document.createElement("li");
   const todoDate = document.createElement("span")
+  //for each that that appends list element to list container, converts array.id to date and adds it as text to span, adds array.text to list element, adds id to list element, sets items in localstorage and removes value from input for each item in the array, mostly used when rendering stored localstorage items
   todoList.forEach((e) => {
     let d = new Date(e.id)
     todoListContainer.append(todoElement);
@@ -60,6 +66,7 @@ function addList() {
     localStorage.setItem("list", JSON.stringify(todoList));
     todoInput.value = ""
   });
+  //removes element by way of index and splice, set updated array in localstorage
   todoElement.addEventListener("click", function() {
     let index = (todoList) => todoList.id == todoElement.id
     let removeIndex = todoList.findIndex(index)
@@ -69,14 +76,14 @@ function addList() {
     todoDate.remove()
     localStorage.setItem("list", JSON.stringify(todoList));
   });
-  
+  //clears entire array, localstorage and html elements
   clearBtn.addEventListener("click", function () {
     localStorage.clear();
     todoList = [];
     todoListContainer.removeChild(todoElement);
   });
 }
-
+//creates array from all li elements currently on page and indexes with search input value to find the html element with matching text
 todoSearch.addEventListener("keyup", () => {
 let searchInput = todoSearch.value.toUpperCase()
 let li = todoContainer.getElementsByTagName("li")
